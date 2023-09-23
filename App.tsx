cc838a95e4f894add4d1bb73196bc6da62a8c93e
Jacob Arvidsson <jacob@theurbancoder.com>
2023-09-23 13:10:56 +0100

    chore: start rn codebase

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
